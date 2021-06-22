c0b310f1083fbeccc981a2dce9f01694b1c7bb64
MartinKays <MartinKayz@users.noreply.github.com>
2021-06-22 14:52:26 +0300

    Simple program to calculate the end times from a given time

---
+++ calculating_end_time.py
@@ -0,0 +1,11 @@
+﻿hour = int(input("Starting time (hours): "))
+mins = int(input("Starting time (minutes): "))
+dura = int(input("Event duration (minutes): "))
+
+# Write your code here.
+
+new_hour = ((hour + dura // 60) + (mins + dura%60) // 60)% 24
+
+new_mins = (mins + dura % 60) % 60
+
+print(str(new_hour) + ":" + str(new_mins))
