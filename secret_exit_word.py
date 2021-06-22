74f35d10afe2cd436bccfe11c293e4bf311ffaa6
MartinKays <MartinKayz@users.noreply.github.com>
2021-06-22 17:55:18 +0300

    will you enter the secret key word

---
+++ secret_exit_word.py
@@ -0,0 +1,10 @@
+﻿secret_exit = "dog"
+
+word = input("Enter a word of to exit the dungeon...")
+
+while word != secret_exit:
+    
+    word = input("Enter the secret word to exit this loop  ")
+    if word == secret_exit:
+        break
+print("You've successfully left the loop.")
