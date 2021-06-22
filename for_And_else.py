7e069ae2e2be9b13e7089db1fa4e1c1efd387ce6
MartinKays <MartinKayz@users.noreply.github.com>
2021-06-22 17:52:58 +0300

    using the else statement with the for loop

---
+++ for_And_else.py
@@ -0,0 +1,14 @@
+﻿for i in range(5):
+    print(i)
+else:
+    # i retains its last value
+    print("else:", i)
+
+# here the for is not even executed    
+j = 111
+for j in range(2,1):
+    print(j)
+else:
+    print("else:", j)
+
+
