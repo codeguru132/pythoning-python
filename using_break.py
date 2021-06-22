cfe30000296f65da32f4bac5e0bbfe30ce148d80
MartinKays <MartinKayz@users.noreply.github.com>
2021-06-22 17:57:06 +0300

    how does break statement wrk

---
+++ using_break.py
@@ -0,0 +1,18 @@
+﻿largest_number = -99999999
+counter = 0
+
+while True:
+    number = int(input("Enter a number or type -1 to end the program"))
+    # this will make the while loop false
+    # like if we put -1 the break will terminate the loop
+    if number == -1:
+        break
+    counter += 1
+    if number > largest_number:
+        largest_number = number
+        
+
+if counter != 0:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
