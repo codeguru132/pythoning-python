78ce70ebddc315ad489d16fd56c209c1c5d7af6d
MartinKays <MartinKayz@users.noreply.github.com>
2021-06-24 00:44:22 +0300

    flat pyramid

---
+++ pyramid_problem.py
@@ -0,0 +1,14 @@
+﻿blocks = int(input("Enter the number of blocks: "))
+
+height = 0
+#
+# Write your code here.
+#	
+layers_between = 1
+while layers_between <= blocks:
+    height += 1
+    blocks -= layers_between
+    layers_between += 1
+     
+
+print("The height of the pyramid:", height)
