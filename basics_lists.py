f018ad9dd475cc0d7f0ac96bf39142738bfba845
Martinkayz <kubonamanrtinyafesi132@gmail.com>
2021-06-29 00:14:20 +0300

    just covered the basics of lists

---
+++ basics_lists.py
@@ -0,0 +1,13 @@
+﻿hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+# Step 1: write a line of code that prompts the user
+# to replace the middle number with an integer number entered by the user.li
+hat_list[2] = int(input("ENter a number here: ..."))
+
+# Step 2: write a line of code that removes the last element from the list.
+del hat_list[-1]
+
+# Step 3: write a line of code that prints the length of the existing list.
+print("\nLength of the list is: ...", len(hat_list))
+
+print(hat_list)
